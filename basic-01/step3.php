d3e4867708d0214aa635883f260ee3d6d2c3fed8
JadRifai19 <jad.rami.rifaii19@gmail.com>
2023-02-18 23:28:46 +0200

    step3

---
+++ basic-01/step3.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Jad Rifaii";
+$email = "jad.rami.rifaii19@example.com";
+echo 'My name is "' . $name . '" and my email is "' . $email . '".<br>';
+
+$height = 10;
+$width = 20;
+$area = $height * $width;
+echo "The area is " . $area . ".";
+?>
